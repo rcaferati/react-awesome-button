1dfaba7a490e474b74d99a533eb877ffae3fe1bf
Rafael Caferati <rafael@caferati.me>
2018-09-03 21:47:31 +0100

    add loki for visual regression test

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import 'loki/configure-react';
 import { configureViewport } from '@storybook/addon-viewport';
 
 // automatically import all files ending in *.stories.js
